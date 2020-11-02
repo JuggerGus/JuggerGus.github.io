a4eafaaefd7715b0ab1c7934ff6c852a2747afdf
jugger7 <gusano2398@gmail.com>
2020-11-02 13:10:29 -0600

    Laptop Gustavo 2 de noviembre

--- script.js
+++ script.js
@@ -1,76 +1,27 @@
+const loadPlaces = function (coords) {
+    // COMMENT FOLLOWING LINE IF YOU WANT TO USE STATIC DATA AND ADD COORDINATES IN THE FOLLOWING 'PLACES' ARRAY
+    //const method = 'api';
 
-window.onload = () => {
-    let method = 'dynamic';
-
-    // if you want to statically add places, de-comment following line:
-    method = 'static';
-    if (method === 'static') {
-        let places = staticLoadPlaces();
-        return renderPlaces(places);
-    }
-
-    if (method !== 'static') {
-        // first get current user location
-        return navigator.geolocation.getCurrentPosition(function (position) {
-
-            // than use it to load from remote APIs some places nearby
-            dynamicLoadPlaces(position.coords)
-                .then((places) => {
-                    renderPlaces(places);
-                })
-        },
-            (err) => console.error('Error in retrieving position', err),
-            {
-                enableHighAccuracy: true,
-                maximumAge: 0,
-                timeout: 27000,
+    const PLACES = [
+        {
+            name: "Fruteria",
+            location: {
+                lat: 19.715469, // add here latitude if using static data
+                lng: -103.465902, // add here longitude if using static data
             }
-        );
+        },
+    ];
+
+    if (method === 'api') {
+        return loadPlaceFromAPIs(coords);
     }
+
+    return Promise.resolve(PLACES);
 };
 
-
-
-
-function staticLoadPlaces() {
-    return [
-        {
-            name: "Habitacion Gustavo",
-            location: {
-                lat: 19.71481, // change here latitude if using static data
-                lng: -103.46468, // change here longitude if using static data
-            }
-        },
-        
-        {
-            name: 'Laboratorio Cusur',
-            location: {
-                lat: 19.7255073,
-                lng: -103.460633,
-            }
-        },
-
-        
-
-        
-
-    ];
-}
-
-
-
-
-
-
-
-
-
-
-
-
 // getting places from REST APIs
-function dynamicLoadPlaces(position) {
-    let params = {
+function loadPlaceFromAPIs(position) {
+    const params = {
         radius: 300,    // search places not farther than this value (in meters)
         clientId: 'HKAW5VITMAJNCPSOD3ADM3GGWN4SPT2MGPYAZLDWWOZRRNLZ',
         clientSecret: 'OCR21ENMN54X5JO4BIK3X2OOMNKDJYPJTJ3D0KFDXGBQ2WSI',
@@ -78,10 +29,10 @@ function dynamicLoadPlaces(position) {
     };
 
     // CORS Proxy to avoid CORS problems
-    let corsProxy = 'https://cors-anywhere.herokuapp.com/';
+    const corsProxy = 'https://cors-anywhere.herokuapp.com/';
 
     // Foursquare API
-    let endpoint = `${corsProxy}https://api.foursquare.com/v2/venues/search?intent=checkin
+    const endpoint = `${corsProxy}https://api.foursquare.com/v2/venues/search?intent=checkin
         &ll=${position.latitude},${position.longitude}
         &radius=${params.radius}
         &client_id=${params.clientId}
@@ -100,40 +51,40 @@ function dynamicLoadPlaces(position) {
         })
 };
 
-function renderPlaces(places) {
-    let scene = document.querySelector('a-scene');
 
-    places.forEach((place) => {
-        let latitude = place.location.lat;
-        let longitude = place.location.lng;
+window.onload = () => {
+    const scene = document.querySelector('a-scene');
 
-        // add place name
-       // let text = document.createElement('a-link');
-       // text.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
-       // text.setAttribute('title', place.name);
-        text.setAttribute('href', 'http://www.example.com/');
-        // text.setAttribute('scale', '10 10 10');
+    // first get current user location
+    return navigator.geolocation.getCurrentPosition(function (position) {
 
+        // than use it to load from remote APIs some places nearby
+        loadPlaces(position.coords)
+            .then((places) => {
+                places.forEach((place) => {
+                    const latitude = place.location.lat;
+                    const longitude = place.location.lng;
 
-        // add place icon
-        const icon = document.createElement('a-image');
-        icon.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude}`);
-        icon.setAttribute('name', place.name);
-        icon.setAttribute('src', './map-marker.png');
+                    // add place name
+                    const text = document.createElement('a-link');
+                    text.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+                    text.setAttribute('title', place.name);
+                    text.setAttribute('href', 'http://www.example.com/');
+                    text.setAttribute('scale', '20 20 20');
 
+                    text.addEventListener('loaded', () => {
+                        window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
+                    });
 
-        icon.setAttribute('scale', '30, 30, 30');
-
-        icon.addEventListener('loaded', () => {window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
-         });
-
-
-
-        text.addEventListener('loaded', () => {
-            window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
-        });
-
-        scene.appendChild(text);
-        scene.appendChild(icon);
-    });
-}
+                    scene.appendChild(text);
+                });
+            })
+    },
+        (err) => console.error('Error in retrieving position', err),
+        {
+            enableHighAccuracy: true,
+            maximumAge: 0,
+            timeout: 27000,
+        }
+    );
+};
