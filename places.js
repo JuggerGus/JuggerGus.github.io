d5292fd922fcff5f166c1c161c941fc75d35063c
jugger7 <gusano2398@gmail.com>
2020-11-04 11:35:14 -0600

    Laptop Gustavo 4 de noviembre

--- places.js
+++ places.js
@@ -1,138 +1,46 @@
-
 window.onload = () => {
-    //let method = 'dynamic';
-
-    // if you want to statically add places, de-comment following line:
-    method = 'static';
-    if (method === 'static') {
-        let places = staticLoadPlaces();
-        return renderPlaces(places);
-    }
-
-    if (method !== 'static') {
-        // first get current user location
-        return navigator.geolocation.getCurrentPosition(function (position) {
-
-            // than use it to load from remote APIs some places nearby
-            dynamicLoadPlaces(position.coords)
-                .then((places) => {
-                    renderPlaces(places);
-                })
-        },
-            (err) => console.error('Error in retrieving position', err),
-            {
-                enableHighAccuracy: true,
-                maximumAge: 0,
-                timeout: 27000,
-            }
-        );
-    }
+    let places = staticLoadPlaces();
+    renderPlaces(places);
 };
-
-
-
 
 function staticLoadPlaces() {
-    return [
-        {
-            name: "Rectoria",
-            location: {
-                lat: 19.725263, // change here latitude if using static data
-                lng: -103.4611392, // change here longitude if using static data
-            }
-        },
+   return [
+       {
+           name: 'Laboratorio',
+           location: {
+               lat: 44.496470,
+               lng: 11.320180,
+           }
+       },
 
-        {
-            name: "Laboratorio",
-            location: {
-                lat: 19.7255073, // change here latitude if using static data
-                lng: -103.460633, // change here longitude if using static data
-            }
-        },
-
-
-        
-
-    ];
+       {
+        name: 'Rectoria',
+        location: {
+            lat: 44.496470,
+            lng: 11.320180,
+        }
+    },
+   ];
 }
 
-
-
-
-
-
-
-
-
-
-
-
-// getting places from REST APIs
-function dynamicLoadPlaces(position) {
-    let params = {
-        radius: 300,    // search places not farther than this value (in meters)
-        clientId: 'HKAW5VITMAJNCPSOD3ADM3GGWN4SPT2MGPYAZLDWWOZRRNLZ',
-        clientSecret: 'OCR21ENMN54X5JO4BIK3X2OOMNKDJYPJTJ3D0KFDXGBQ2WSI',
-        version: '20300101',    // foursquare versioning, required but unuseful for this demo
-    };
-
-    // CORS Proxy to avoid CORS problems
-    let corsProxy = 'https://cors-anywhere.herokuapp.com/';
-
-    // Foursquare API
-    let endpoint = `${corsProxy}https://api.foursquare.com/v2/venues/search?intent=checkin
-        &ll=${position.latitude},${position.longitude}
-        &radius=${params.radius}
-        &client_id=${params.clientId}
-        &client_secret=${params.clientSecret}
-        &limit=10
-        &v=${params.version}`;
-    return fetch(endpoint)
-        .then((res) => {
-            return res.json()
-                .then((resp) => {
-                    return resp.response.venues;
-                })
-        })
-        .catch((err) => {
-            console.error('Error with places API', err);
-        })
-};
-
 function renderPlaces(places) {
-    let scene = document.querySelector('a-scene');
+   let scene = document.querySelector('a-scene');
 
-    places.forEach((place) => {
-        let latitude = place.location.lat;
-        let longitude = place.location.lng;
+   places.forEach((place) => {
+       let latitude = place.location.lat;
+       let longitude = place.location.lng;
 
-        // add place name
-       //let text = document.createElement('a-link');
-       //text.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
-       //text.setAttribute('title', place.name);
-        text.setAttribute('href', 'http://www.example.com/');
-        //text.setAttribute('scale', '10 10 10');
+       let model = document.createElement('a-entity');
+       model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+       model.setAttribute('gltf-model', './assets/magnemite/scene.gltf');
+       model.setAttribute('rotation', '0 -45 0');
+       model.setAttribute('animation-mixer', '');
+       model.setAttribute('scale', '0.5 0.5 0.5');
 
+       model.addEventListener('loaded', () => {
+           window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
+       });
 
-        // add place icon
-        const icon = document.createElement('a-image');
-        icon.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude}`);
-        icon.setAttribute('name', place.name);
-        icon.setAttribute('src', './map-marker.png');
-
-
-        icon.setAttribute('scale', '30, 30, 30');
-
-        icon.addEventListener('loaded', () => {window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
-         });
-
-
-
-        text.addEventListener('loaded', () => {
-            window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
-        });
-
-        scene.appendChild(text);
-        scene.appendChild(icon);
-    });
+       scene.appendChild(model);
+   });
 }
