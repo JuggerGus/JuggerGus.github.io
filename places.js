6916933281cc69d76dacf3cd39fb1d9d4d2006a5
jugger7 <gusano2398@gmail.com>
2020-11-09 09:55:43 -0600

    Laptop Gustavo 9 de noviembre

--- places.js
+++ places.js
@@ -123,7 +123,7 @@ window.onload = () => {
                     text.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
                     text.setAttribute('title', place.name);
                     text.setAttribute('href', 'http://www.cusur.udg.mx/es/');
-                    text.setAttribute('scale', '10 10 10');
+                    text.setAttribute('scale', '5 5 5');
 
                     // add place icon
                     //const icon = document.createElement('a-image');
